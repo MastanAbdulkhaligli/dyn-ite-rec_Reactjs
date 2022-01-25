ca5111a8045cea0938ba1166456c81a90d87141d
MastanAbdulkhaligli <mastanabdulkhaligli@gmail.com>
2022-01-26 03:39:21 +0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// For Router example
-import App2 from "./Router/App2";
-import Page from "./Pagination/Page";
-
-ReactDOM.render(<App2 />, document.getElementById("root"));
